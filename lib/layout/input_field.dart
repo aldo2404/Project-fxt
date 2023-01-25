0d7ddf50640819c2a32d56ecc19ce7a77aa14ebb
Nixon <nixon@thinq24.com>
2023-01-25 18:37:22 +0530

    checkbox create and tried email validation

--- lib/layout/input_field.dart
+++ lib/layout/input_field.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
 
 class TextFields {
-  inputLine(String text, Color inputfieldcolor) {
-    return Container(
-      height: 40,
-      width: 300,
-      // margin: EdgeInsets.all(8),
-      // padding: EdgeInsets.all(5),
-
-      // decoration: BoxDecoration(
-      //   borderRadius: BorderRadius.circular(5.0),
-      // ),
-
-      // color: Colors.white,
-      child: TextField(
-        textAlign: TextAlign.center,
-        decoration: InputDecoration(
-            filled: true,
-            fillColor: inputfieldcolor,
-            border: OutlineInputBorder(),
-            hintText: text,
-            hintStyle: TextStyle(fontSize: 14)),
+  inputLine(String text, Color inputfieldcolor,
+      [bool? password, TextEditingController? controller]) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 40,
+        width: 300,
+        child: TextFormField(
+          keyboardType: TextInputType.emailAddress,
+          controller: controller,
+          obscureText: password!,
+          enableSuggestions: password,
+          autocorrect: password,
+          textAlign: TextAlign.center,
+          decoration: InputDecoration(
+              filled: true,
+              fillColor: inputfieldcolor,
+              border: OutlineInputBorder(),
+              hintText: text,
+              hintStyle: TextStyle(fontSize: 14)),
+        ),
       ),
     );
   }
