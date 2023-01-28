6b2c61412e5bee462169d01c779f971653961682
Nixon <nixon@thinq24.com>
2023-01-28 18:24:30 +0530

    email validtion added and alignment adjusted

--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -16,6 +16,7 @@ class _LoginPageState extends State<LoginPage> {
   final emailcontroller = TextEditingController();
   final passcontroller = TextEditingController();
   bool _isChecked = false;
+  late String email;
 
   @override
   Widget build(BuildContext context) {
@@ -34,52 +35,60 @@ class _LoginPageState extends State<LoginPage> {
                 children: [
                   SizedBox(height: 65),
                   Container(
-                    height: 110,
-                    width: 99,
+                    height: 100,
+                    width: 110,
                     child: Image.asset(
                       "assets/image/splashlogo.png",
                       fit: BoxFit.fitWidth,
                     ),
                   ),
-                  Container(
-                    child: Text("Welcome back!",
-                        style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.white)),
-                  ),
+                  const Text("Welcome back!",
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white)),
                   SizedBox(height: 20),
-                  TextFields(
+                  ReuseTextFields(
+                    keyboardtypes: TextInputType.emailAddress,
                     text: "Username",
                     inputfieldcolor: Colors.white,
                     password: false,
                     controller: emailcontroller,
                     validate: ((value) {
-                      if (value!.isEmpty ||
-                          !RegExp(r"^[\w-\.]+@([\w-]+\.)+[\w]")
-                              .hasMatch(value)) {
-                        return 'Enter correct email id';
+                      if (value!.isEmpty) {
+                        return "Enter the mail id";
+                      } else if (!RegExp(r"^[\w-\.]+@([\w-]+\.)+[\w]")
+                          .hasMatch(value)) {
+                        return "Enter valid email";
                       }
                       return null;
                     }),
+                    onsaved: (value) {
+                      email = value!;
+                    },
                   ),
-                  SizedBox(height: 17),
-                  TextFields(
+
+                  ReuseTextFields(
+                    keyboardtypes: TextInputType.visiblePassword,
                     text: "Password",
                     inputfieldcolor: Colors.white,
                     password: true,
                     controller: passcontroller,
                     validate: ((value) {
                       if (value!.isEmpty) {
-                        return 'required *';
+                        return "required *";
                       } else if (value.length < 6) {
                         return 'password length too short';
+                      } else if (value.length > 15) {
+                        return "password length too long";
+                      } else {
+                        return null;
                       }
                     }),
                   ),
-                  //SizedBox(height: 17),
-                  Container(
-                    height: 40,
+
+                  SizedBox(
+                    height: 35,
                     width: 300,
                     child: GestureDetector(
                       child: Row(
@@ -92,7 +101,7 @@ class _LoginPageState extends State<LoginPage> {
                               _isChecked = !value!;
                             }),
                           ),
-                          Text(
+                          const Text(
                             "Remember me",
                             style: TextStyle(color: Colors.white),
                           ),
@@ -105,12 +114,16 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                   GestureDetector(
                     onTap: () {
-                      setState(() {});
+                      setState(() {
+                        if (formfield.currentState!.validate()) {}
+                      });
                     },
-                    child: Buttonfield().clickButton("Log In"),
+                    child: Buttonfield().clickButton(
+                      "Log In",
+                    ),
                   ),
                   //SizedBox(height: 13),
-                  Container(
+                  const SizedBox(
                     height: 25,
                     width: 300,
                     child: Text(
@@ -123,12 +136,18 @@ class _LoginPageState extends State<LoginPage> {
                     onTap: () {},
                     child: Buttonfield().clickButton("Log In with SSO"),
                   ),
-                  SizedBox(height: 120),
-                  Container(
+                  const SizedBox(height: 120),
+                  SizedBox(
                     height: 40,
                     width: 300,
                     child: GestureDetector(
-                      child: Text(
+                      onTap: (() {
+                        setState(() {
+                          Navigator.of(context)
+                              .pushNamed('/passwordforgotpage');
+                        });
+                      }),
+                      child: const Text(
                         "Forgot Password?",
                         style: TextStyle(color: Colors.white),
                         textAlign: TextAlign.center,
